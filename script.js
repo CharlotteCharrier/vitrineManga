0c0fd7776f58f7b70317d878de296bf065751a20
JCMaif <158159856+JCMaif@users.noreply.github.com>
2024-02-22 09:20:08 +0100

    landing page Accueil on default loading

--- script.js
+++ script.js
@@ -96,8 +96,8 @@ window.addEventListener('load', async () => {
     let contactPageBtn = document.querySelector('.contact');
 
     const displayContactPage = () => {
-        hydratePage.className="contact";
-        hydratePage.innerHTML = 
+        hydratePage.className = "contact";
+        hydratePage.innerHTML =
             `<form method="post">
                 <input type="text" placeholder="Nom - Prénom">
                 <input type="text" placeholder="Adresse mail">
@@ -105,7 +105,7 @@ window.addEventListener('load', async () => {
                 <button type="submit">Envoyer</button>
                 <img src="./assets/contact_img.png">
             </form>`
-    } 
+    }
 
     contactPageBtn.addEventListener("click", () => {
         displayContactPage();
@@ -115,9 +115,9 @@ window.addEventListener('load', async () => {
     let presentationPageBtn = document.querySelector('.presentation');
 
     const displayPresentationPage = () => {
-        hydratePage.className="presentation";
-        hydratePage.innerHTML = 
-        `<div><img src="./assets/promised-neverland-presentation.jpg" alt="les personnages principaux">
+        hydratePage.className = "presentation";
+        hydratePage.innerHTML =
+            `<div><img src="./assets/promised-neverland-presentation.jpg" alt="les personnages principaux">
         <img src="./assets/img_presentation.png">
         <p>Emma et ses frères et sœurs, tous orphelins, ont été placés dans un établissement spécialisé lorsqu'ils étaient tout jeunes. Bien que leur liberté soit limitée et que les règles soient parfois un peu strictes, les enfants mènent une vie heureuse à Grace Field House, et la femme qu'ils nomment « Maman » s'occupe d'eux et leur offre tout l'amour qu'une mère pourrait offrir à ses enfants. Un soir, après le départ d'une fille appelé Conny, Emma et Norman découvrent que les enfants de cet orphelinat sont en réalité du bétail. Ils sont élevés dans une ferme à l'allure d'orphelinat, et livrés à des monstres avec le consentement de « Maman ». Ils devront faire preuve d'ingéniosité pour survivre et tenter de s'échapper.</p>
         </div>`
@@ -131,15 +131,15 @@ window.addEventListener('load', async () => {
     let landingPageBtn = document.querySelector('.landing');
 
     const displayLandingPage = () => {
-        hydratePage.className="landing";
-        hydratePage.innerHTML = 
-        `<div>
+        hydratePage.className = "landing";
+        hydratePage.innerHTML =
+            `<div>
         <img src="./assets/landing_img.jpg" alt="image du manga The Promised Neverland">
         <p>Emma et ses frères et sœurs, tous orphelins, ont été placés dans un établissement spécialisé lorsqu'ils étaient tout jeunes. Bien que leur liberté soit limitée et que les règles soient parfois un peu strictes, les enfants mènent une vie heureuse à Grace Field House, et la femme qu'ils nomment « Maman » s'occupe d'eux et leur offre tout l'amour qu'une mère pourrait offrir à ses enfants. Un soir, après le départ d'une fille appelé Conny, Emma et Norman découvrent que les enfants de cet orphelinat sont en réalité du bétail. Ils sont élevés dans une ferme à l'allure d'orphelinat, et livrés à des monstres avec le consentement de « Maman ». Ils devront faire preuve d'ingéniosité pour survivre et tenter de s'échapper.</p>
         <button>Découvrir</button>
         </div>`
     }
-
+    displayLandingPage();
     landingPageBtn.addEventListener("click", () => {
         displayLandingPage();
     })
